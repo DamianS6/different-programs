ef6aa419a47ffe499841d038d799da3b82d7f3f4
DamianS6 <damianszczepanski1996@gmail.com>
2019-05-29 01:03:16 +0200

    Update backup_to_zip.py with new functionalities
    Add an option to archive files with certain extensions, except certain
    extensions and to change path where the backup is created.

--- backup_to_zip.py
+++ backup_to_zip.py
@@ -1,17 +1,31 @@
-"""A program that copies an entire folder and its contents
-into a ZIP file whose filename increments."""
+"""
+A program that copies an entire folder and its contents
+into a ZIP file whose filename increments.
+
+folder - where to look for files to copy
+restrict - copy just files with certain extensions
+exclude - copy files except those with certain extensions
+directory - path where you want the copy to be created, optional;
+			by default current program directory
+
+'restrict' and 'exclude' are optional and have to be lists with strings like '.xxx'.
+Obviously it doesn't make much sense to give these two optional arguments in the same run.
+"""
 
 import zipfile
 import os
 
 
-def zip_backup(folder):
-	# Backup the entire contents of "folder" into a ZIP file.
+def zip_backup(folder, restrict=None, exclude=None, directory=None):
 
-	folder = os.path.abspath(folder)    # make sure folder is absolute
+	# If path is given, changes the directory where copy will be created.
+	if directory:
+		os.chdir(os.path.abspath(directory))
 
-	# Figure out the filename this code should use based on
-	# what files already exist.
+	# Make sure folder is absolute.
+	folder = os.path.abspath(folder)
+
+	# Figure out the filename this code should use based on what files already exist.
 	n = 1
 	while True:
 		zip_filename = os.path.basename(folder) + '_' + str(n) + '.zip'
@@ -20,20 +34,36 @@ def zip_backup(folder):
 		n += 1
 
 	# Create the ZIP file.
-	print('Creating %s...' % zip_filename)
+	print(f'Creating {zip_filename}...')
 	backup_zip = zipfile.ZipFile(zip_filename, 'w')
 
 	# Walk the entire folder tree and compress the files in each folder.
 	for foldername, subfolders, filenames in os.walk(folder):
-		print('Adding files in %s...' % foldername)
+		print(f'Adding files in {foldername}...')
+
 		# Add the current folder to the ZIP file.
 		backup_zip.write(foldername)
-		# Add all the files in this folder to the ZIP file.
+
 		for filename in filenames:
+			# Don't backup the backup ZIP files.
 			newbase = os.path.basename(folder) + '_'
 			if filename.startswith(newbase) and filename.endswith('.zip'):
 				continue
-			# don't backup the backup ZIP files
-			backup_zip.write(os.path.join(foldername, filename))
+
+			if restrict:
+				for ext in restrict:
+					if filename.endswith(ext):
+						backup_zip.write(os.path.join(foldername, filename))
+
+			elif exclude:
+				for ext in exclude:
+					if filename.endswith(ext):
+						continue
+					backup_zip.write(os.path.join(foldername, filename))
+
+			# If neither restrict nor exclude is given, copy all the files.
+			else:
+				backup_zip.write(os.path.join(foldername, filename))
+
 	backup_zip.close()
 	print('Done.')
