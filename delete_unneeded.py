e05db6e57dda8f793d3d2fb4c65d6ac940bca4da
DamianS6 <damianszczepanski1996@gmail.com>
2019-05-29 11:16:11 +0200

    Add delete_unneeded.py; fix typo in selective_copy
    delete_unneeded - Looks for files larger than given size in given
    directory.

---
+++ delete_unneeded.py
@@ -0,0 +1,21 @@
+"""A program that looks for files larger than given size (100MB by default)."""
+
+import os
+
+
+def delete_unneeded(folder, size='100MB'):
+	sizes = {
+		'kb': 1024,
+		'mb': 1048576,
+		'gb': 1073741824,
+	}
+	num_size = sizes[size[-2:].lower()] * float(size[:-2])
+
+	print('Files and folders larger than %s in %s:' % (size, folder))
+	for dirpath, dirnames, filenames in os.walk(folder):
+		try:
+			for filename in filenames:
+				if os.path.getsize(os.path.join(dirpath, filename)) > num_size:
+					print(os.path.join(dirpath, filename))
+		except FileNotFoundError:
+			continue
